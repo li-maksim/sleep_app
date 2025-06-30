b1bf8bd2044329e9fcdfad4994565c661efa8951
li-maksim <max.mail4@yandex.ru>
2025-06-30 19:42:55 +0300

    Added titles to icons
    Fixed bug with recoloring of icons
    Began working on Manage Section

--- src/components/IconsSlider.jsx
+++ src/components/IconsSlider.jsx
@@ -8,8 +8,14 @@ function IconsSlider({icons, direction}) {
         const str = arr.join('')
         const alt = str.replace('_', ' ')
 
+        let className = 'icon '
+        if (i % 2 === 0) {
+            className = className + 'alt_color'
+        }
+
         return (
-            <div key={e.text + i} className="icon">
+            <div key={e.text + i} className={className}>
+                <div className="icon_title">{alt}</div>
                 <img src={e.src} alt={alt} />
             </div>
         )
@@ -21,6 +27,9 @@ function IconsSlider({icons, direction}) {
                 {rowOfIcons}
             </div>
             <div className={`icons ${direction}`}>
+                <div className="icon_title">
+                    
+                </div>
                 {rowOfIcons}
             </div>
         </div>
---
+++ src/components/ManageSection.jsx
@@ -0,0 +1,18 @@
+import '../styles/ManageSection.css'
+
+function ManageSection() {
+
+    return(
+        <section className="section manage">
+            <div className="container">
+                <h2 className="h2">Manage anxiety and trouble sleeping</h2>
+                <p className="p main_text">
+                    We’ll help you <span className="purple_text">wake up energized</span> by providing a variety of meditation sessions designed to reduce stress. Whether you’re winding down for the night or need a boost during the day, you’ll find the perfect collection of playlists tailored to any case. Enjoy calming soundscapes and guided practices for <span className="purple_text">deep rest</span> and empowering sleep.
+                </p>
+
+            </div>
+        </section>
+    )
+}
+
+export default ManageSection
