75ed31e81996d26737637c1c0375d87e3118dab8
li-maksim <max.mail4@yandex.ru>
2025-08-27 21:07:17 +0300

    Started working on bg in Manage section
    Fixed bug with card pseudo element
    Switched kitten images to match design

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import '../styles/App.css'
+import '../styles/bg.css'
 import Header from './Header'
 import Home from './Home'
 import CollectionSection from './CollectionSection'
@@ -15,22 +16,30 @@ function App() {
   return (
     < >
     <div className="bg">
+      <div className="bg_home">
         <div className="bg_big_circle"></div>
-        <div className="bg_pink_circles">
-          <div className="bg_circle pink_circle"></div>
-          <div className="bg_circle pink_circle"></div>
-        </div>
-        <div className="bg_blue_circles">
-          <div className="bg_circle blue_circle"></div>
-          <div className="bg_circle blue_circle"></div>
-          <div className="bg_circle blue_circle"></div>
-        </div>
-        <div className="bg_red_circles">
-          <div className="bg_circle red_circle"></div>
-          <div className="bg_circle red_circle"></div>
-          <div className="bg_circle red_circle"></div>
-        </div>
-        <div className="bg_wave"></div>
+          <div className="bg_pink_circles">
+            <div className="bg_circle pink_circle"></div>
+            <div className="bg_circle pink_circle"></div>
+          </div>
+          <div className="bg_blue_circles">
+            <div className="bg_circle blue_circle"></div>
+            <div className="bg_circle blue_circle"></div>
+            <div className="bg_circle blue_circle"></div>
+          </div>
+          <div className="bg_red_circles">
+            <div className="bg_circle red_circle"></div>
+            <div className="bg_circle red_circle"></div>
+            <div className="bg_circle red_circle"></div>
+          </div>
+          <div className="bg_wave bg_home_wave"></div>
+      </div>
+      <div className="bg_manage">
+        <div className="bg_circle light_blue_circle"></div>
+        <div className="bg_big_circle"></div>
+        <div className="bg_circle yellow_circle"></div>
+        <div className="bg_circle red_circle"></div>
+      </div>
     </div>
       <div className="app">
         <Header />
