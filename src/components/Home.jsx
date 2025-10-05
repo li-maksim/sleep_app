1822175cdce03dbb39e1ffb931433e3faf788f9c
li-maksim <max.mail4@yandex.ru>
2025-10-05 17:18:26 +0300

    Adaptation for even smaller screens
    Adapted to max-width of 320px

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home({ btnFn }) {
         <section className="section home">
             <div className="container home">
                 <h1>Fall asleep faster <br /><span>wake up easier</span></h1>
-                <p className="p">Try living life to the fullest and resting well with our sleep sounds</p>
+                <p className="p main_text">Try living life to the fullest and resting well with our sleep sounds</p>
                 <div className="home_img_wrapper">
                     <img className="img home_img" src={img} alt="iPhone 15 image" />
                 </div>
